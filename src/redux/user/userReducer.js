377e4ed5c0548b0a0c0ceeb6352b47cf062df80e
aslamjon ibragimov <aslamjon.ibragimov@gmail.com>
2021-09-20 03:30:48 -0900

    added initial data

--- src/redux/user/userReducer.js
+++ src/redux/user/userReducer.js
@@ -1,7 +1,16 @@
 import { UserTypes } from "./userType";
 
 const INITIAL_STATE = {
-    currentUser: {}
+    currentUser: {
+        is_admin: false,
+        token: '',
+        user: {
+            balance: 0,
+            phone_number: "915411998",
+            full_name: "Aslamjon",
+            card_number: null
+        }
+    }
 }
 
 const UserReduser = (state = INITIAL_STATE, action) => {
